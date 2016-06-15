e59d5779936ca2afd5c63c3af554ddc7ad6fd93c
ignasiet <ignasi.af@gmail.com>
2016-06-15 10:13:44 -0300

    Correction in causal graph: avoid repeated states

--- src/planner/CausalGraph.java
+++ src/planner/CausalGraph.java
@@ -2,9 +2,12 @@ package planner;
 
 import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashSet;
 import java.util.Hashtable;
+import java.util.Set;
 
 import pddlElements.Action;
+import pddlElements.Axiom;
 import pddlElements.Domain;
 import pddlElements.Effect;
 
@@ -21,10 +24,36 @@ public class CausalGraph {
 		e = antecessor.keys();
 		while(e.hasMoreElements()){
 			String key = e.nextElement().toString();
-			ArrayList<String> list = antecessor.get(key);
+			ArrayList<String> list = (ArrayList<String>) antecessor.get(key).clone();
 			construct(key, list);
 		}
+        extractAxioms(d);
 	}
+
+    private void extractAxioms(Domain d){
+        for(Axiom ax : d._Axioms){
+            ArrayList<String> ant = new ArrayList<String>(ax._Body);
+            for(String p : ax._Head)
+            if(antecessor.containsKey(p)){
+                Set<String> hs = new HashSet<>();
+                hs.addAll(ant);
+                hs.addAll(antecessor.get(p));
+                ant.clear();
+                ant.addAll(hs);
+                antecessor.put(p, ant);
+            }else{
+                antecessor.put(p, ant);
+            }
+        }
+    }
+
+    private void construct(Domain d){
+        for(Axiom ax : d._Axioms){
+            for(String p : ax._Head){
+
+            }
+        }
+    }
 
 	private void extract(Action a) {
 		for(Effect e : a._Effects){
@@ -32,7 +61,13 @@ public class CausalGraph {
 				ArrayList<String> list = new ArrayList<String>(a._precond);
 				list.addAll(e._Condition);
 				if(antecessor.containsKey(effect)){
-					list.addAll(antecessor.get(effect));
+					//list.addAll(antecessor.get(effect));
+					//Remove duplicates
+					Set<String> hs = new HashSet<>();
+					hs.addAll(list);
+					hs.addAll(antecessor.get(effect));
+					list.clear();
+					list.addAll(hs);
 					antecessor.put(effect, list);
 				}else{
 					antecessor.put(effect, list);
@@ -44,8 +79,12 @@ public class CausalGraph {
 	private void construct(String key, ArrayList<String> list){
 		for(String s : list){
 			if(sucessors.containsKey(s)){
-				ArrayList<String> sucList = new ArrayList<String>(sucessors.get(s));
-				sucList.add(key);
+				//Remove duplicates
+				Set<String> hs = new HashSet<>();
+				hs.addAll(sucessors.get(s));
+				ArrayList<String> sucList = new ArrayList<String>();
+				hs.add(key);
+				sucList.addAll(hs);
 				sucessors.put(s, sucList);
 			}else{
 				ArrayList<String> sucList = new ArrayList<String>();
