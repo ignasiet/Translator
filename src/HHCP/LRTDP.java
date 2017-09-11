3a7eb8f974755cbe84ed38d66ffe491d38eb104d
ignasiet <ignasi.af@gmail.com>
2017-09-11 19:34:27 -0300

    Costs heuristic corrected

---
+++ src/HHCP/LRTDP.java
@@ -0,0 +1,7 @@
+package HHCP;
+
+/**
+ * Created by ignasi on 11/09/17.
+ */
+public class LRTDP {
+}
---
+++ src/causalgraph/VertexNodeId.java
@@ -0,0 +1,7 @@
+package causalgraph;
+
+/**
+ * Created by ignasi on 11/09/17.
+ */
+public class VertexNodeId {
+}
